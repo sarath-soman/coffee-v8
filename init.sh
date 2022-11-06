08bdaa82b8181ebc3086e06331b7a81892b8628e
Sarath Soman <nsarathsoman@gmail.com>
2022-11-06 13:38:55 +0000

    parameterise stable version and arch

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 chmod +x scripts/fetch_v8.sh
-V8_STABLE_VERSION=10.7.193.18
+export V8_STABLE_VERSION=10.7.193.18
+export ARCH=arm64.release
 ./scripts/fetch_v8.sh
--- scripts/pull_v8.sh
+++ scripts/pull_v8.sh
@@ -1,10 +1,12 @@
 cd libs
-mkdir v8
-cd v8
+mkdir google
+cd google
 fetch v8
 cd v8
 git config branch.autosetupmerge always
 git config branch.autosetuprebase always
 git fetch
+echo "v8 Version: $V8_STABLE_VERSION, Arch $ARCH"
 git checkout $V8_STABLE_VERSION
 gclient sync
+./tools/dev/gm.py $ARCH
